942a2042e27b975f9371ef68e42c600aa7c284d5
Nguyen Nhat Nam (jamienguyen) <nguyennhatnam11022003@gmail.com>
2025-10-25 02:05:51 +0700

    ac

--- MinimalGridPath.cpp
+++ MinimalGridPath.cpp
@@ -1,75 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
 int n; 
 vector<string> a;
-
-const int N = 3002;
-int f[N][N];
-
-vector<int> bucket[N];
-vector<int> sted;
-
+const int N = 3003;
+ 
+int vis[N][N];
+ 
 int main() {
     cin >> n;
     a.assign(n + 1, "");
     for (int i=1; i<=n; ++i) {
         cin >> a[i];
         a[i] =  " " + a[i];
-        bucket[i].reserve(n);
     }
-
-    f[n][n] = 1;
-    for (int max_bucket=0,xy = n+n-1; xy >=2; --xy) {
-
-        sted.clear();
-        for (int r,x=1,y=xy-1; x<=n && y>=1; x++,y--) {
-            if (y > n) continue;
-
-            r = N;
-            if (x + 1 <= n) 
-                r = f[x+1][y];
-            if (y + 1 <= n) 
-                r = min(r, f[x][y+1]);
-            
-            bucket[r].push_back(x);
-            max_bucket = max(r, max_bucket);
-        }
-        for (int i=1; i<=max_bucket; ++i) {
-            for (int e : bucket[i]) {
-                sted.push_back(e);
+ 
+ 
+    vector<pair<int , int> > potential = {{1, 1}}, new_potential = {};
+    string ans;
+    ans.push_back(a[1][1]);
+    for (int xy=2; xy<n+n; ++xy) {
+ 
+        char bestC = 'Z';
+        for (auto [x, y] : potential) {
+            if (x < n) {
+                if (a[x+1][y] < bestC) {
+                    bestC = a[x+1][y];
+                } 
             }
-            bucket[i].clear();
-        }
-
-        for (int x : sted) {
-            bucket[a[x][xy-x]].push_back(x);
-        }
-        sted.clear();
-        for (int c='A'; c<='Z'; ++c) {
-            for (int e: bucket[c]) {
-                sted.push_back(e);
+            if (y < n) {
+                if (a[x][y+1] < bestC) {
+                    bestC = a[x][y+1];
+                }
             }
-            bucket[c].clear();
         }
-
-        for (int i=0; i<sted.size(); ++i) {
-            f[sted[i]][xy-sted[i]] = i + 1;
+        for (auto [x, y] : potential) {
+            if (x < n) {
+                if (a[x+1][y] == bestC) {
+                    if (!vis[x+1][y]) 
+                    {
+                        vis[x+1][y] = true;
+                        new_potential.push_back({x+1, y});
+                    }
+                }
+            }
+            if (y < n) {
+                if (a[x][y+1] == bestC) {
+                    if (vis[x][y+1]) continue;
+                    vis[x][y+1] = true;
+                    new_potential.push_back({x, y+1});
+                }
+            }
         }
+        ans.push_back(bestC);
+        swap(new_potential, potential);
+        new_potential={};
     }
-
-    int x=1,y=1;
-    while (x!=n || y!=n) {
-        cout << a[x][y];
-        if (x==n) y++;
-        else if (y==n) x++;
-        else {
-            if (f[x][y+1] < f[x+1][y])
-                y++;
-            else 
-                x++;
-        }
-    }
-    cout << a[n][n];
+    cout << ans;
     return 0;
 }
---
+++ stub/3359/test.py
@@ -0,0 +1,8 @@
+n=int(input()) 
+a=[ str(input()) for _ in range(n)]
+
+m = 11
+print(m)
+for x in range(m):
+    print(a[x][:m])
+
